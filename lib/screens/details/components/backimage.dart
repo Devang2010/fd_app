53cffb1a5b3eb6bd0ba00ee980ce8e5592b0ac91
Anubhav <anubhav.11803451@lpu.in>
2020-07-25 22:37:52 +0530

    Update backimage.dart

--- lib/screens/details/components/backimage.dart
+++ lib/screens/details/components/backimage.dart
@@ -10,22 +10,28 @@ class BackImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List images = [
+      'assets/images/img2.jpg',
+      'assets/images/img1.jpg',
+      'assets/images/moon1.jpg',
+      'assets/images/img4.jpg',
+    ];
     return Container(
-      height: size.height,
-      width: size.width,
+      // height: size.height,
+      // width: size.width,
       alignment: Alignment.topCenter,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 3,
+        itemCount: images.length,
         itemBuilder: (BuildContext context, int index) {
           return Container(
-            height: 240,
-            width: size.width,
+            // height: 280,
+            width: size.width * 1.1,
             alignment: Alignment.topCenter,
             child: Image.asset(
-              "assets/images/img2.jpg",
-              // fit: BoxFit.cover,
-              // height: 523.7,
+              images[index],
+              // fit: BoxFit.contain,
+              // height: 533.7,
             ),
           );
         },
