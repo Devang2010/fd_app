ad5708efdf12ad4f9074509c847f0f2cdb0d0da9
Anubhav <anubhav.11803451@lpu.in>
2020-07-25 19:03:06 +0530

    Update paymentbody.dart

--- lib/screens/payment/components/paymentbody.dart
+++ lib/screens/payment/components/paymentbody.dart
@@ -1,6 +1,15 @@
+import 'package:fd_app/screens/payment/components/amountbox.dart';
+import 'package:fd_app/screens/payment/components/cardbox.dart';
+import 'package:fd_app/screens/payment/components/donatenow.dart';
+import 'package:fd_app/screens/payment/components/walletbox.dart';
 import 'package:flutter/material.dart';
 
-class PaymentBody extends StatelessWidget {
+class PaymentBody extends StatefulWidget {
+  @override
+  _PaymentBodyState createState() => _PaymentBodyState();
+}
+
+class _PaymentBodyState extends State<PaymentBody> {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -23,142 +32,9 @@ class PaymentBody extends StatelessWidget {
           Align(
             heightFactor: 9,
             alignment: Alignment.bottomCenter,
-            child: Container(
-              height: 50.0,
-              // width: 250,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30.0),
-                color: Colors.black,
-              ),
-              child: FlatButton(
-                onPressed: () {},
-                padding: EdgeInsets.symmetric(horizontal: 80.0),
-                child: Text(
-                  "Donate Now",
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.w300),
-                ),
-              ),
-            ),
+            child: DonatenowButton(),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class CardBox extends StatelessWidget {
-  const CardBox({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 55.0,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        color: Colors.white54.withBlue(255),
-        border: Border.all(color: Colors.black26),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(6.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Icon(
-                Icons.call_to_action,
-                size: 19.0,
-              ),
-            ),
-            Text("Harsh"),
-            SizedBox(width: 130.0),
-            Text(
-              "...3580",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Container(
-              width: 40,
-              child: Icon(
-                Icons.check,
-                size: 16,
-                color: Colors.green,
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Walletbox extends StatelessWidget {
-  const Walletbox({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 55.0,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        color: Colors.yellow[100].withOpacity(0.5),
-        border: Border.all(color: Colors.black26),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(6.0),
-        child: Row(
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Icon(
-                Icons.account_balance_wallet,
-                size: 20.0,
-              ),
-            ),
-            SizedBox(width: 13.0),
-            Text("Wallet"),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Amountbox extends StatelessWidget {
-  const Amountbox({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 55.0,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        color: Colors.black12,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Text("Amount"),
-            SizedBox(width: 180.0),
-            Text(
-              "₹6,000",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Icon(
-              Icons.arrow_drop_down,
-              color: Colors.black45,
-            ),
-          ],
-        ),
       ),
     );
   }
