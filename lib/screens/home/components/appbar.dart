99cdc7f8cef118a3cd78a5f4ed13ee187d45253f
Anubhav <anubhav.11803451@lpu.in>
2020-07-25 19:02:22 +0530

    Update appbar.dart

--- lib/screens/home/components/appbar.dart
+++ lib/screens/home/components/appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Buildappbar extends StatelessWidget {
   const Buildappbar({
@@ -14,7 +15,7 @@ class Buildappbar extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Container(
-            height: size.height * 0.055,
+            height: size.width * 0.13,
             width: size.width * 0.75,
             decoration: BoxDecoration(
               color: Colors.white,
@@ -33,18 +34,35 @@ class Buildappbar extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Icon(Icons.menu),
-                  Text("Find Your Intrest"),
+                  SvgPicture.asset(
+                    "assets/svgs/menu.svg",
+                    height: 18.0,
+                  ),
+                  Text(
+                    "Find campagin, charities",
+                    style: TextStyle(color: Colors.black38),
+                  ),
                   Icon(Icons.search)
                 ],
               ),
             ),
           ),
-          CircleAvatar(
-            backgroundColor: Colors.black,
-            child: Icon(
-              Icons.person_outline,
-              color: Colors.white,
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.orange[200],
+                  blurRadius: 10.0,
+                  spreadRadius: -1.5,
+                  offset: Offset(0, 6),
+                ),
+              ],
+              color: Colors.orange[200],
+            ),
+            child: SvgPicture.asset(
+              'assets/svgs/avatar.svg',
+              height: 44.0,
             ),
           ),
         ],
