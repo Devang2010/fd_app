0f6ae623c65b659909d00a385e88dfa2ae55e52e
Anubhav <anubhav.11803451@lpu.in>
2020-07-25 19:02:25 +0530

    Update categorytab.dart

--- lib/screens/home/components/categorytab.dart
+++ lib/screens/home/components/categorytab.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
-class CategoryTab extends StatelessWidget {
+class CategoryTab extends StatefulWidget {
   const CategoryTab({
     Key key,
   }) : super(key: key);
 
   @override
+  _CategoryTabState createState() => _CategoryTabState();
+}
+
+class _CategoryTabState extends State<CategoryTab> {
+  @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    List categories = [
+      'Medical',
+      'Education',
+      'Animal',
+      'Science',
+      'Nature',
+    ];
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -17,30 +29,30 @@ class CategoryTab extends StatelessWidget {
             "Categories",
             style: TextStyle(
               fontSize: 18.0,
-              fontWeight: FontWeight.w600,
+              fontWeight: FontWeight.bold,
+              letterSpacing: -0.5,
             ),
           ),
         ),
         Container(
           margin: EdgeInsets.symmetric(vertical: 10.0),
-          height: size.height * 0.055,
+          height: size.width * 0.13,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
-            itemCount: 5,
+            itemCount: categories.length,
             itemBuilder: (BuildContext context, int index) {
               return Container(
+                alignment: Alignment.center,
                 margin: EdgeInsets.only(left: 20.0),
                 height: size.height * 0.055,
                 width: size.width * 0.25,
                 decoration: BoxDecoration(
-                  color: Colors.orangeAccent,
+                  color: index.isEven ? Colors.amber : Colors.orange,
                   borderRadius: BorderRadius.circular(15.0),
                 ),
-                child: Center(
-                  child: Text(
-                    "Edata",
-                    style: TextStyle(fontSize: 15.0),
-                  ),
+                child: Text(
+                  categories[index],
+                  style: TextStyle(fontSize: 15.0),
                 ),
               );
             },
