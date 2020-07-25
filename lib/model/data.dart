5661e0db6123df4e67d4e4c8621e14bdd3d6c55c
Anubhav <anubhav.11803451@lpu.in>
2020-07-25 22:37:50 +0530

    Update data.dart

--- lib/model/data.dart
+++ lib/model/data.dart
@@ -7,24 +7,68 @@ class Story {
 List<Story> stories = [
   Story(
     image: [
-      'assets/images/img4.jpg',
+      'assets/images/boy.jpg',
       'assets/images/img3.jpg',
     ],
     tag1: [
-      'Nature',
-      'Nat',
+      'Inspiring',
+      'Water',
     ],
     tag2: [
-      'Jelly',
-      'Jey',
+      'Day',
+      'Green',
     ],
     title: [
-      'An amazingly beautiful jelly',
-      'An amazingly be',
+      'A Boy that helps 169 children in west',
+      'Want to spend a day beside beach',
     ],
     subtitle: [
-      'Its was a rare click caught on the camera of matt.ferr',
-      'Its was a rare click caught on err',
+      'When its come to looking your best, there are some standard tools of the trade',
+      'These beach has that vibe which could possibly steal your stress.',
+    ],
+  ),
+  Story(
+    image: [
+      'assets/images/moon1.jpg',
+      'assets/images/img1.jpg',
+    ],
+    tag1: [
+      'Cosmos',
+      'Happy',
+    ],
+    tag2: [
+      'Moon',
+      'Heart',
+    ],
+    title: [
+      'An astronomical body orbiting Earth',
+      'A 200 year old bone structure',
+    ],
+    subtitle: [
+      'It is the fifth-largest satellite in the Solar System, and by far the largest among planetary satellites.',
+      'This skelton is 200 years old, here are some facts.',
+    ],
+  ),
+  Story(
+    image: [
+      'assets/images/heart.jpg',
+      'assets/images/moon.jpg',
+    ],
+    tag1: [
+      'Heart',
+      'Fact',
+    ],
+    tag2: [
+      'Healty',
+      'Antique',
+    ],
+    title: [
+      'A muscular organ about the size of a fist',
+      'Cool and Calm Moon',
+    ],
+    subtitle: [
+      'It is located just behind and slightly left of the breastbone, pumps blood through through artries',
+      'If learning to be calm and cool, just think of The Moon.',
     ],
   ),
 ];
